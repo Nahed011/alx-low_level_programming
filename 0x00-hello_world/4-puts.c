8413294f0c4d56797d93dcf66a80bbfba613ead6
Nahed011 <nahedlamloum@gmail.com>
2023-06-16 13:03:11 -0700

    task3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <STDIO.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (success)
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle"):
+    return (0):
+}
