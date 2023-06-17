c9e11ba3b8e3e6c8ff5439d9600baa6aea7a7b91
Nahed011 <nahedlamloum@gmail.com>
2023-06-17 16:02:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program that prints exactly with proper grammar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
