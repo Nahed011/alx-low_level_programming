06804b5a489402a6b01141be1d31bcd768d4cf3f
Nahed011 <nahedlamloum@gmail.com>
2023-07-01 07:22:55 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -36,7 +36,8 @@ void printHexes(char *b, int start, int end)
 }
 
 /**
- * printASCII - print ascii values for string b, to replace with '.'
+ * printASCII - print ascii values for string b,
+ * formatted to replace nonprintable chars with '.'
  * @b: string to print
  * @start: starting position
  * @end: ending position
@@ -49,7 +50,7 @@ void printASCII(char *b, int start, int end)
 	while (i < end)
 	{
 		ch = *(b + i + start);
-		if (!isorinttableASCII(ch))
+		if (!isprintableASCII(ch))
 			ch = 46;
 		printf("%c", ch);
 		i++;
@@ -71,7 +72,7 @@ void print_buffer(char *b, int size)
 		for (start = 0; start < size < size; start += 10)
 		{
 			end = (size - start < 10) ? size - start : 10;
-			printf("%08x; ", start);
+			printf("%08x: ", start);
 			printHexes(b, start, end);
 			printASCII(b, start, end);
 			printf("\n");
