bf46f73c45ef1d1f5ad1a59bb2a3591c6a56d4dd
Nahed011 <nahedlamloum@gmail.com>
2023-07-03 15:26:08 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,7 +6,7 @@
  * @haystack: input
  * @needle: input
  * Return: a pointer to the beginning of the located substring,
-
+ * or NULL if the substring is not found
 */
 
 char *_strstr(char *haystack, char *needle)
