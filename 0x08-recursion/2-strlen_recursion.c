1bad9042d9488ee3ab41dd439c903bed3a76ad26
Nahed011 <nahedlamloum@gmail.com>
2023-07-05 10:05:36 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string.
  * @s: string
+ * Return: int
 */
 
 int _strlen_recursion(char *s)
@@ -13,4 +14,5 @@ int _strlen_recursion(char *s)
 	{
 		ntr += _strlen_recursion(s + 1) + 1;
 	}
+	return (ntr);
 }
