419e4af11dbdbc69074092987967b033fce78eec
Nahed011 <nahedlamloum@gmail.com>
2023-07-09 06:07:11 -0700

    3

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ *
+ * @s: string to print
+ *
+ * Return: nothing
+*/
+
+void _puts(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s + 48);
+		s++;
+	}
+	_putchar('\n');
+}
