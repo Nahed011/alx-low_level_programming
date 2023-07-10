acd83d79cd05e32f85c5d48b1f62af98a226d7b3
Nahed011 <nahedlamloum@gmail.com>
2023-07-10 07:54:24 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,19 +10,20 @@
 char *_strdup(char *str)
 {
 	int i = 0, size = 0;
+	char *m;
 
 	if (str == NULL)
 		return (NULL);
 
 	for (; str[size] != '\0'; size++)
-		;
+	;
 
 	/* +1 puts the end of string character on the size*/
 	m = malloc(size * sizeof(*str) + 1);
 
 	if (m == 0)
 		return (NULL);
-	if else
+	while
 	{
 		for (; i < size; i++)
 			m[i] = str[i];
