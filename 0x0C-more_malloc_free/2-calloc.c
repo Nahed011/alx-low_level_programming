fbcca5edc30dbe7bdf0413c4fbf48e264fe45639
Nahed011 <nahedlamloum@gmail.com>
2023-07-13 16:18:40 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * _memset - fills memory with a constant byte
@@ -13,11 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *pyr = s;
+	char *ptr = s;
 
 	while (n--)
 		*s++ = b;
-	return (s);
+	return (ptr);
 }
 
 /**
