03f33ec8d012dc093830c95b6e6f043763f6eedc
Nahed011 <nahedlamloum@gmail.com>
2023-07-19 11:13:56 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
-	
+
 	if (array && size && cmp)
 		while (i < size)
 		{
@@ -18,4 +18,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 				return (i);
 			i++;
 		}
+	return (-1);
 }
