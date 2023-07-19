1f0e778eda4354f3553d62f150051edda934309b
Nahed011 <nahedlamloum@gmail.com>
2023-07-19 11:10:01 -0700

    u

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,13 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0
-		if (array && size && cmp)
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
-				i++;
-			}
+	int i = 0;
+	
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
 }
