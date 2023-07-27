450f9462b2a533d0fe7f5154daf2c7bb3db20f85
Nahed011 <nahedlamloum@gmail.com>
2023-07-27 15:29:17 -0700

    l

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,5 +20,9 @@ list_t *add_node(list_t **head, const char *str)
 			free(new_head);
 			return (NULL);
 		}
+		new_head->len = _strlen(new_head->str);
 	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
 }
